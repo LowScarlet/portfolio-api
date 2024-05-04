635c669f9a9a4fceda15b261c237e1188de603b0
LowScarlet <namaetohyaakiradesu@gmail.com>
2024-05-05 00:18:54 +0700

    Init Frontend Website

--- backend/src/Start.js
+++ backend/src/Start.js
@@ -4,5 +4,5 @@ const port = process.env.PORT || 5000;
 const node = process.env.NODE_ENV || 'dev';
 
 app.listen(port, () => {
-  console.log(`[${node.toUpperCase()}] Listening: http://localhost:${port}`);
+  console.log(`[${node}] Listening: http://localhost:${port}`);
 });
