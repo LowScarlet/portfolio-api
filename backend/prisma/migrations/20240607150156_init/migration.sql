600c5008005b487135e4fe785b8cc8824c1b577c
LowScarlet <namaetohyaakiradesu@gmail.com>
2024-06-07 22:34:08 +0700

    !

--- backend/prisma/migrations/20240607150156_init/migration.sql
+++ backend/prisma/migrations/20240607150156_init/migration.sql
@@ -59,12 +59,22 @@ CREATE TABLE "UserProfile" (
 -- CreateTable
 CREATE TABLE "Portfolio" (
     "id" TEXT NOT NULL,
-    "title" TEXT NOT NULL,
-    "desc" TEXT,
-    "image" TEXT,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "isPublic" BOOLEAN NOT NULL DEFAULT true,
+    "ownerId" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Portfolio_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "PortfolioProfile" (
+    "id" TEXT NOT NULL,
+    "logo" TEXT,
     "banner" TEXT,
-    "isPublic" BOOLEAN NOT NULL DEFAULT false,
-    "name" TEXT,
+    "fullName" TEXT,
     "label" TEXT,
     "nickname" TEXT,
     "about" TEXT,
@@ -72,11 +82,11 @@ CREATE TABLE "Portfolio" (
     "email" TEXT,
     "phone" TEXT,
     "website" TEXT,
-    "ownerId" TEXT,
+    "porfolioId" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "Portfolio_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "PortfolioProfile_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -103,6 +113,75 @@ CREATE TABLE "PortfolioComment" (
     CONSTRAINT "PortfolioComment_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "SocialMedia" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "SocialMedia_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "PortfolioConnect" (
+    "id" TEXT NOT NULL,
+    "identifier" TEXT NOT NULL,
+    "socialMediaId" INTEGER NOT NULL,
+    "portfolioId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "PortfolioConnect_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "TechincalSkill" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "TechincalSkill_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Institution" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "website" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Institution_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "PortfolioEducation" (
+    "id" SERIAL NOT NULL,
+    "score" DOUBLE PRECISION NOT NULL,
+    "institutionId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "PortfolioEducation_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_PinnedByUser" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "_PortfolioToTechincalSkill" (
+    "A" TEXT NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
 
@@ -119,7 +198,22 @@ CREATE UNIQUE INDEX "RefreshToken_hashedToken_key" ON "RefreshToken"("hashedToke
 CREATE UNIQUE INDEX "UserProfile_userId_key" ON "UserProfile"("userId");
 
 -- CreateIndex
+CREATE UNIQUE INDEX "PortfolioProfile_porfolioId_key" ON "PortfolioProfile"("porfolioId");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "PortfolioVote_userId_portfolioId_key" ON "PortfolioVote"("userId", "portfolioId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_PinnedByUser_AB_unique" ON "_PinnedByUser"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_PinnedByUser_B_index" ON "_PinnedByUser"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_PortfolioToTechincalSkill_AB_unique" ON "_PortfolioToTechincalSkill"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_PortfolioToTechincalSkill_B_index" ON "_PortfolioToTechincalSkill"("B");
 
 -- AddForeignKey
 ALTER TABLE "OneTimePassword" ADD CONSTRAINT "OneTimePassword_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
@@ -134,6 +228,9 @@ ALTER TABLE "UserProfile" ADD CONSTRAINT "UserProfile_userId_fkey" FOREIGN KEY (
 ALTER TABLE "Portfolio" ADD CONSTRAINT "Portfolio_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
+ALTER TABLE "PortfolioProfile" ADD CONSTRAINT "PortfolioProfile_porfolioId_fkey" FOREIGN KEY ("porfolioId") REFERENCES "Portfolio"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE "PortfolioVote" ADD CONSTRAINT "PortfolioVote_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
@@ -144,3 +241,24 @@ ALTER TABLE "PortfolioComment" ADD CONSTRAINT "PortfolioComment_userId_fkey" FOR
 
 -- AddForeignKey
 ALTER TABLE "PortfolioComment" ADD CONSTRAINT "PortfolioComment_portfolioId_fkey" FOREIGN KEY ("portfolioId") REFERENCES "Portfolio"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PortfolioConnect" ADD CONSTRAINT "PortfolioConnect_socialMediaId_fkey" FOREIGN KEY ("socialMediaId") REFERENCES "SocialMedia"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PortfolioConnect" ADD CONSTRAINT "PortfolioConnect_portfolioId_fkey" FOREIGN KEY ("portfolioId") REFERENCES "Portfolio"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PortfolioEducation" ADD CONSTRAINT "PortfolioEducation_institutionId_fkey" FOREIGN KEY ("institutionId") REFERENCES "Institution"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_PinnedByUser" ADD CONSTRAINT "_PinnedByUser_A_fkey" FOREIGN KEY ("A") REFERENCES "Portfolio"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_PinnedByUser" ADD CONSTRAINT "_PinnedByUser_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_PortfolioToTechincalSkill" ADD CONSTRAINT "_PortfolioToTechincalSkill_A_fkey" FOREIGN KEY ("A") REFERENCES "Portfolio"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_PortfolioToTechincalSkill" ADD CONSTRAINT "_PortfolioToTechincalSkill_B_fkey" FOREIGN KEY ("B") REFERENCES "TechincalSkill"("id") ON DELETE CASCADE ON UPDATE CASCADE;
